8e8dc671fbd6cabae49b7c70beeb1119d6f37732
ErikMCM <70036542+ErikMCM@users.noreply.github.com>
2021-01-01 12:38:39 -0600

    Create RPI_Project.lua

---
+++ assets/RPI_Project.lua
@@ -0,0 +1,14 @@
+local RPI = {}
+
+RPI.Info = {
+	["Name"] = nil;
+	["Description"] = nil;
+	["Version"] = nil;
+	["Author"] = {nil, nil};
+	["License"] = nil;
+	["Respository"] = nil;
+}
+
+RPI.Dependencies = {}
+
+return RPI
